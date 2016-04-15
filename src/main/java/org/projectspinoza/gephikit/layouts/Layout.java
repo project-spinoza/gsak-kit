811054bd0398f12d980f3a1f609dc57533007010
adnan <ad.sahibzada@gmail.com>
2016-04-15 14:35:46 +0500

    gephi service with vertx

---
+++ src/main/java/org/projectspinoza/gephikit/layouts/Layout.java
@@ -0,0 +1,23 @@
+package org.projectspinoza.gephikit.layouts;
+
+public class Layout {
+	private YifanHuLayout yiFanHuLayout;
+	private FruchtermanReingold fruchtermanReingold;
+
+	public YifanHuLayout getYiFanHuLayout() {
+		return yiFanHuLayout;
+	}
+
+	public void setYiFanHuLayout(YifanHuLayout yiFanHuLayout) {
+		this.yiFanHuLayout = yiFanHuLayout;
+	}
+
+	public FruchtermanReingold getFruchtermanReingold() {
+		return fruchtermanReingold;
+	}
+
+	public void setFruchtermanReingold(FruchtermanReingold fruchtermanReingold) {
+		this.fruchtermanReingold = fruchtermanReingold;
+	}
+   
+}
---
+++ src/main/java/org/projectspinoza/gephikit/layouts/YifanHuLayout.java
@@ -0,0 +1,33 @@
+package org.projectspinoza.gephikit.layouts;
+
+public class YifanHuLayout{
+  
+	String name;
+	int  iteration;
+	int distance;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getIteration() {
+		return iteration;
+	}
+
+	public void setIteration(int iteration) {
+		this.iteration = iteration;
+	}
+	
+	public int getDistance() {
+		return distance;
+	}
+
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+	
+}
